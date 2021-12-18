f6c7d945703e3daffc126d1515d4ede937f5e864
difanta <jackiedifant@gmail.com>
2021-12-18 20:20:06 +0100

    login working

--- php/home.php
+++ php/home.php
@@ -4,10 +4,22 @@ session_start();
 
 include "login.php";
 
-$htmlPage = file_get_contents("../HTML/home.html");
 
-Login::handle_login($htmlPage);
+if(isset($_POST["method"])) {
+    // handle login/register/logout POST request
+    Login::handle_login();
 
-echo $htmlPage;
+    // redirect to same page (it will use GET request) https://en.wikipedia.org/wiki/Post/Redirect/Get
+    header("HTTP/1.1 303 See Other");
+    header("Location: ./home.php");
+} else /* GET */ {
+    $htmlPage = file_get_contents("../HTML/home.html");
+
+    // show login/register/logout results
+    Login::set_login_contents($htmlPage);
+
+    echo $htmlPage;
+}
+
 
 ?>
--- php/login.php
+++ php/login.php
@@ -37,23 +37,32 @@ class Login {
     private static $p_login_section = "<div id=\"loginSection\" class=\"slidableIn";
     private static $p_signup_section = "<div id=\"signupSection\" class=\"slidableIn";
 
-    private static $p_if_logged = "placeholder=\"iflogged\"";        // show if logged
-    private static $p_if_not_logged = "placeholder=\"ifnotlogged\""; // show if not logged
+    private static $p_if_logged = "iflogged";                           // show if logged
+    private static $p_if_not_logged = "ifnotlogged";                    // show if not logged
+
+    private static $p_if_login_success = "ifloginsuccess";              // show if login succeded
+    private static $p_if_not_login_success = "ifnotloginsuccess";       // show if login failed
+
+    private static $p_if_register_success = "ifregistersuccess";        // show if register succeded
+    private static $p_if_not_register_success = "ifnotregistersuccess"; // show if register failed
 
     private static function display_account_buttons(&$htmlPage, $_is_logged) {
+        $htmlPage = str_replace("placeholder", "style", $htmlPage);
         if($_is_logged) {
-            $htmlPage = str_replace(Login::$p_if_logged     , ""                         ,    $htmlPage); // show
-            $htmlPage = str_replace(Login::$p_if_not_logged , " style=\"display: none\"" ,    $htmlPage); // hide
+            $htmlPage = str_replace(Login::$p_if_logged     , ""              ,    $htmlPage); // show
+            $htmlPage = str_replace(Login::$p_if_not_logged , "display: none" ,    $htmlPage); // hide
         } else {
-            $htmlPage = str_replace(Login::$p_if_logged     , " style=\"display: none\"" ,    $htmlPage); // hide
-            $htmlPage = str_replace(Login::$p_if_not_logged , ""                         ,    $htmlPage); // show
+            $htmlPage = str_replace(Login::$p_if_logged     , "display: none" ,    $htmlPage); // hide
+            $htmlPage = str_replace(Login::$p_if_not_logged , ""              ,    $htmlPage); // show
         }
     }
 
     public static function is_logged() {
-        if(!isset($_SESSION["login"])) { return false; }
+        return isset($_SESSION["login"]);
 
-        $connection = new DBAccess();
+        // no need to verify with db since $_SESSION is not modifiable by the user
+
+        /*$connection = new DBAccess();
         $connectionOk = $connection->openDB();
 
         if($connectionOk) {
@@ -64,13 +73,15 @@ class Login {
             { return false; }
         } else {
             echo "connection error";
-        }
+        }*/
     }
 
     public static function is_logged_admin() {
-        if(!isset($_SESSION["login"])) { return false; }
+        return (Login::is_logged() && $_SESSION["is_admin"] == true);
 
-        $connection = new DBAccess();
+        // no need to verify with db since $_SESSION is not modifiable by the user
+
+        /*$connection = new DBAccess();
         $connectionOk = $connection->openDB();
 
         if($connectionOk) {
@@ -81,79 +92,144 @@ class Login {
             { return false; }
         } else {
             echo "connection error";
+        }*/
+    }
+
+    public static function handle_login() {
+        if(!Login::is_logged()) // if not logged try to login or register
+        {
+            if($_POST["method"] == "login") 
+            {
+                $email = $_POST["email"];
+                $password = $_POST["password"];
+
+                $connection = new DBAccess();
+                $connectionOk = $connection->openDB();
+
+                if($connectionOk) {
+                    $result = $connection->get("SELECT * FROM Utente WHERE Utente.email = '$email'");
+                    if($result && ($result["password"] == $password)) 
+                         { $_SESSION["success"] = true  ; $_SESSION["login"] = $result["id"]; $_SESSION["is_admin"] = $result["admin"]; } 
+                    else { $_SESSION["success"] = false ; }
+                    $connection->closeConnection();
+                } 
+                else /* Connection not Ok */
+                { $_SESSION["success"] = false; }
+
+                if($_SESSION["success"] == false) 
+                {   /* save data to avoid form reset */
+                    $_SESSION["email"] = $_POST["email"];
+                }
+            } 
+            elseif ($_POST["method"] == "register") 
+            {
+                $nome = $_POST["nome"];
+                $cognome = $_POST["cognome"];
+                $data_di_nascita = $_POST["data_di_nascita"];
+                $email = $_POST["email"];
+                $password = $_POST["password"];
+
+                $connection = new DBAccess();
+                $connectionOk = $connection->openDB();
+
+                if($connectionOk) 
+                {
+                    if($connection->insert("INSERT INTO Utente(nome, cognome, data_di_nascita, email, password, admin)
+                                            VALUES ('$nome', '$cognome', '$data_di_nascita', '$email', '$password', '0')"))
+                         { $_SESSION["success"] = true  ; } 
+                    else { $_SESSION["success"] = false ; }
+                    $connection->closeConnection();
+                } 
+                else /* Connection not Ok */
+                { $_SESSION["success"] = false; }
+
+                if($_SESSION["success"] == false) 
+                {   /* save data to avoid form reset */
+                    $_SESSION["nome"] = $_POST["nome"];
+                    $_SESSION["cognome"] = $_POST["cognome"];
+                    $_SESSION["data_di_nascita"] = $_POST["data_di_nascita"];
+                    $_SESSION["email"] = $_POST["email"];
+                }
+            }
+        } 
+        else /* Not logged: you can only logout */
+        { 
+            if($_POST["method"] == "logout") {
+                session_unset();
+                session_destroy();
+                session_start();
+                $_SESSION["success"] = true;
+            }
+        }
+        if(isset($_SESSION["success"])) {
+            /* remember method if something happened*/
+            $_SESSION["method"] = $_POST["method"];
         }
     }
 
-    public static function handle_login(&$htmlPage) {
-        if(isset($_POST["method"])) // #1 if login method is defined go on (= if a login form button was clicked)
+    public static function set_login_contents(&$htmlPage) {
+        if(isset($_SESSION["method"]))
         {
-            if(!Login::is_logged()) //    if not logged try to login or register
+            $method = $_SESSION["method"];
+            $success = $_SESSION["success"];
+
+            switch ($method) 
             {
-                if($_POST["method"] == "login") 
-                {
-                    $email = $_POST["email"];
-                    $password = $_POST["password"];
-
-                    $connection = new DBAccess();
-                    $connectionOk = $connection->openDB();
-
-                    if($connectionOk) {
-                        $result = $connection->get("SELECT * FROM Utente WHERE Utente.email = '$email'");
-                        if($result && ($result["password"] == $password)) {
-                            echo "login done";
-                            $_SESSION["login"] = $result["id"];
-                        } else {
-                            echo "wrong password or email";
-                        }
-                        $connection->closeConnection();
+                case 'login':
+                    $htmlPage = str_replace(Login::$p_account_dropdown, Login::$p_account_dropdown . " class=\"dropdown\"", $htmlPage);
+                    $htmlPage = str_replace(Login::$p_account_section,  Login::$p_account_section  . " slideOut\"",         $htmlPage);
+                    $htmlPage = str_replace(Login::$p_login_section,    Login::$p_login_section    . " slideIn\"",          $htmlPage);
+                    
+                    if($success) {
+                        $htmlPage = str_replace(Login::$p_if_login_success     , ""              , $htmlPage);
+                        $htmlPage = str_replace(Login::$p_if_not_login_success , "display: none" , $htmlPage);
                     } else {
-                        echo "connection error";
+                        $htmlPage = str_replace(Login::$p_if_login_success     , "display: none" , $htmlPage);
+                        $htmlPage = str_replace(Login::$p_if_not_login_success , ""              , $htmlPage);
+                    }                 
+                    
+                    if(!$success) {
+                        unset($_SESSION["email"]);
                     }
-                } 
-                elseif ($_POST["method"] == "register") 
-                {
-                    $nome = $_POST["nome"];
-                    $cognome = $_POST["cognome"];
-                    $data_di_nascita = $_POST["data_di_nascita"];
-                    $email = $_POST["email"];
-                    $password = $_POST["password"];
 
-                    $connection = new DBAccess();
-                    $connectionOk = $connection->openDB();
+                    break;
 
-                    if($connectionOk) {
-                        if($connection->insert("INSERT INTO Utente(nome, cognome, data_di_nascita, email, password, admin)
-                                                VALUES ('$nome', '$cognome', '$data_di_nascita', '$email', '$password', '0')")) {
-                            echo "register done";
-                        } else {
-                            echo "email already used"; // or other input errors, but the input should already be checked earlier
-                        }
-                        $connection->closeConnection();
+                case 'register':
+                    $htmlPage = str_replace(Login::$p_account_dropdown, Login::$p_account_dropdown . " class=\"dropdown\"", $htmlPage);
+                    $htmlPage = str_replace(Login::$p_account_section,  Login::$p_account_section  . " slideOut\"",         $htmlPage);
+                    $htmlPage = str_replace(Login::$p_signup_section,   Login::$p_signup_section   . " slideIn\"",          $htmlPage);
+                    
+                    if($success) {
+                        $htmlPage = str_replace(Login::$p_if_register_success     , ""              , $htmlPage);
+                        $htmlPage = str_replace(Login::$p_if_not_register_success , "display: none" , $htmlPage);
                     } else {
-                        echo "connection error";
+                        $htmlPage = str_replace(Login::$p_if_register_success     , "display: none" , $htmlPage);
+                        $htmlPage = str_replace(Login::$p_if_not_register_success , ""              , $htmlPage);
                     }
-                }
-            } else { // if logged you can only logout
-                if($_POST["method"] == "logout") {
-                    echo "logout done";
-                    session_unset();
-                    session_destroy();
-                }
-            }
 
-            // Keep dropdown state (if #1)
-            if       ($_POST["method"] == "login") {
-                $htmlPage = str_replace(Login::$p_account_dropdown, Login::$p_account_dropdown . " class=\"dropdown\"", $htmlPage);
-                $htmlPage = str_replace(Login::$p_account_section,  Login::$p_account_section  . " slideOut\"",         $htmlPage);
-                $htmlPage = str_replace(Login::$p_login_section,    Login::$p_login_section    . " slideIn\"",          $htmlPage);
-            } elseif ($_POST["method"] == "register") {
-                $htmlPage = str_replace(Login::$p_account_dropdown, Login::$p_account_dropdown . " class=\"dropdown\"", $htmlPage);
-                $htmlPage = str_replace(Login::$p_account_section,  Login::$p_account_section  . " slideOut\"",         $htmlPage);
-                $htmlPage = str_replace(Login::$p_signup_section,   Login::$p_signup_section   . " slideIn\"",          $htmlPage);
-            } elseif ($_POST["method"] == "logout") {
-                $htmlPage = str_replace(Login::$p_account_dropdown, Login::$p_account_dropdown . " class=\"dropdown\"", $htmlPage);
+                    if(!$success) {
+                        unset($_SESSION["nome"]);
+                        unset($_SESSION["cognome"]);
+                        unset($_SESSION["data_di_nascita"]);
+                        unset($_SESSION["email"]);
+                    }
+
+                    break;
+
+                case 'logout':
+                    $htmlPage = str_replace(Login::$p_account_dropdown, Login::$p_account_dropdown . " class=\"dropdown\"", $htmlPage);
+                    
+                    break;
             }
+            unset($_SESSION["method"]);
+            unset($_SESSION["success"]);
         }
+
+        $htmlPage = str_replace(Login::$p_if_login_success        , "display: none" , $htmlPage);
+        $htmlPage = str_replace(Login::$p_if_not_login_success    , ""              , $htmlPage);
+        $htmlPage = str_replace(Login::$p_if_register_success     , "display: none" , $htmlPage);
+        $htmlPage = str_replace(Login::$p_if_not_register_success , ""              , $htmlPage);
+
         // in all cases set login button status
         Login::display_account_buttons($htmlPage, Login::is_logged());
     }
