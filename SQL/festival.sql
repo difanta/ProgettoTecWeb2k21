cfbbb3b48cbfb428f9a4d45533195fda807b0aed
Luca Annicchiarico <luca.annic@gmail.com>
2021-12-19 12:39:51 +0100

    Added DB users
    utente@utente.com:utente123
    admin@admin.com:admin123

--- SQL/festival.sql
+++ SQL/festival.sql
@@ -6,11 +6,11 @@ START TRANSACTION;
 SET
 time_zone = "+00:00";
 
-drop table if exists Utente;
-drop table if exists Film;
 drop table if exists _Like;
-drop table if exists Proiezione;
 drop table if exists Biglietto;
+drop table if exists Proiezione;
+drop table if exists Film;
+drop table if exists Utente;
 
 create table Utente
 (
@@ -23,18 +23,22 @@ create table Utente
     admin           tinyint     not null
 );
 
+insert into Utente(id, nome, cognome, data_di_nascita, email, password, admin)
+values (1, "utente", "utente", 2000 - 01 - 01, "utente@utente.com", "utente123", 0),
+       (2, "admin", "admin", 2000 - 01 - 01, "admin@admin.com", "admin123", 1);
+
 create table Film
 (
-    id          int             not null auto_increment primary key,
-    nome        varchar(20)     not null,
-    descrizione text            not null,
-    durata      int             not null,
-    anno        smallint        not null,
-    regista     varchar(50)     not null,
-    produttore  varchar(50)     not null,
-    cast        text            not null,
-    in_gara     tinyint         not null,
-    approvato   tinyint         not null,
+    id          int         not null auto_increment primary key,
+    nome        varchar(20) not null,
+    descrizione text        not null,
+    durata      int         not null,
+    anno        smallint    not null,
+    regista     varchar(50) not null,
+    produttore  varchar(50) not null,
+    cast        text        not null,
+    in_gara     tinyint     not null,
+    approvato   tinyint     not null,
     candidatore int,
     foreign key (candidatore) references Utente (id) on update cascade on delete set null
 );
