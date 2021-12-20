f9a967e920843f62e28950779f25296e2aa3d438
Luca Annicchiarico <luca.annic@gmail.com>
2021-12-20 20:59:44 +0100

    biglietti pagina utente

---
+++ php/PaginaUtente.php
@@ -0,0 +1,62 @@
+<?php
+
+session_start();
+
+include "login.php";
+use DB\DBAccess;
+
+function printBiglietti(&$htmlPage)
+{
+    $listaBiglietti = "";
+
+    if (Login::is_logged()) {
+
+        $connection = new DBAccess();
+        $connectionOk = $connection->openDB();
+
+        if ($connectionOk) {
+            $biglietti = $connection->get("SELECT Film.nome, Biglietto.id, orario FROM Biglietto join Film join Proiezione join Utente on " . $_SESSION["login"]);
+            $connection->closeConnection();
+            if ($biglietti != null) {
+                $listaBiglietti .= "<ul>";
+                foreach ($biglietti as $biglietto) {
+                    $listaBiglietti .=
+                        "<li class=\"ticket\"><p><span class=\"ticket-up\">" . $biglietto["nome"] .
+                        "</span><span class=\"ticket-up\">#" . $biglietto["id"] .
+                        "</span></p><p><span class=\"ticket-low\">" . $biglietto["orario"] .
+                        "</span></p></li>";
+                }
+                unset($biglietto);
+                $listaBiglietti .= "</ul>";
+            } else {
+                $listaBiglietti .= "<p>Errore load biglietti</p>";
+            }
+        } else {
+            $listaBiglietti .= "<p>Errore connessione db</p>";
+        }
+    } else { // not logged
+        $listaBiglietti .= "<p>Utente non loggato</p>";
+    }
+
+    $htmlPage =  str_replace("<listaBiglietti/>", $listaBiglietti, $htmlPage);
+}
+
+if (isset($_POST["method"])) {
+    // handle login/register/logout POST request
+    Login::handle_login();
+
+    // redirect to same page (it will use GET request) https://en.wikipedia.org/wiki/Post/Redirect/Get
+    header("HTTP/1.1 303 See Other");
+    header("Location: ./PaginaUtente.php");
+} else /* GET */ {
+    $htmlPage = file_get_contents("../HTML/PaginaUtente.html");
+
+    // show login/register/logout results
+    Login::set_login_contents($htmlPage);
+    printBiglietti($htmlPage);
+
+    $htmlPage = str_replace("placeholder", "style", $htmlPage);
+
+    echo $htmlPage;
+}
+
--- php/contest.php
+++ php/contest.php
@@ -1,52 +1,85 @@
 <?php
 
-include_once "db.php";
+session_start();
 
+include "login.php";
 use DB\DBAccess;
 
-$paginaHtml = file_get_contents("contest.html");
+function submitContest(&$htmlPage)
+{
+    $messaggi = "";
+    $titolo = "";
+    $descrizione = "";
+    $linkyt = "";
+    $email = "";
 
-$messaggi = "";
-$titolo = "";
-$descrizione = "";
-$linkyt = "";
-$email = "";
-
-if (isset($_POST["submit"])) {
-    $titolo = $_POST["titolo"];
-    $descrizione = $_POST["descrizione"];
-    $linkyt = $_POST["linkyt"];
-    $email = $_POST["email"];
-
-    if (strlen($titolo) == 0) {
-        $messaggi .= "<li>Titolo non presente</li>";
-    } elseif (preg_match('/\d/', $titolo)) {
-        $messaggi .= "<li>Il nome non può contenere numeri</li>";
-    }
-
-    if (strlen($descrizione) == 0) {
-        $messaggi .= "<li>Descrizione non presente</li>";
-    }
-
-    if (strlen($linkyt) == 0) {
-        $messaggi .= "<li>Descrizione non presente</li>";
-    }
-
-    if (strlen($email) == 0) {
-        $messaggi .= "<li>Descrizione non presente</li>";
-    }
-
-    if (strlen($messaggi) == "") {
+    if (Login::is_logged()) {
 
         $connection = new DBAccess();
         $connectionOk = $connection->openDB();
 
-        if($connectionOk){ // TODO
-            $result = $connection->get("insert into Film(id, nome, descrizione, durata, anno, regista, produttore, cast, in_gara, approvato, candidatore) values (1, \"\");");
-        }
+        if ($connectionOk) {
+            if (isset($_POST["submit"])) {
+                $titolo = $_POST["titolo"];
+                $descrizione = $_POST["descrizione"];
+                $linkyt = $_POST["linkyt"];
+                $email = $_POST["email"];
 
-        $messaggi .= "<p>Candidatura inviata con successo.</p>"
+                if (strlen($titolo) == 0) {
+                    $messaggi .= "<li>Titolo non presente</li>";
+                } elseif (preg_match('/\d/', $titolo)) {
+                    $messaggi .= "<li>Il nome non può contenere numeri</li>";
+                }
+
+                if (strlen($descrizione) == 0) {
+                    $messaggi .= "<li>Descrizione non presente</li>";
+                }
+
+                if (strlen($linkyt) == 0) {
+                    $messaggi .= "<li>Descrizione non presente</li>";
+                }
+
+                if (strlen($email) == 0) {
+                    $messaggi .= "<li>Descrizione non presente</li>";
+                }
+
+                if (strlen($messaggi) == "") {
+
+                    $connection = new DBAccess();
+                    $connectionOk = $connection->openDB();
+
+                    if($connectionOk){ // TODO
+                        $result = $connection->get("insert into Film(id, nome, descrizione, durata, anno, regista, produttore, cast, in_gara, approvato, candidatore) values (1, \"\");");
+                    }
+
+                    $messaggi .= "<p>Candidatura inviata con successo.</p>"
     }
+            }
+        } else {
+            $messaggi .= "<p>Errore connessione db</p>";
+        }
+    } else { // not logged
+        $messaggi .= "<p>Utente non loggato</p>";
+    }
+
+    $htmlPage =  str_replace("<listaBiglietti/>", $messaggi, $htmlPage);
 }
 
-?>
+if (isset($_POST["method"])) {
+    // handle login/register/logout POST request
+    Login::handle_login();
+
+    // redirect to same page (it will use GET request) https://en.wikipedia.org/wiki/Post/Redirect/Get
+    header("HTTP/1.1 303 See Other");
+    header("Location: ./contest.php");
+} else /* GET */ {
+    $htmlPage = file_get_contents("../HTML/contest.html");
+
+    // show login/register/logout results
+    Login::set_login_contents($htmlPage);
+    printBiglietti($htmlPage);
+
+    $htmlPage = str_replace("placeholder", "style", $htmlPage);
+
+    echo $htmlPage;
+}
