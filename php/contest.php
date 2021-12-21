f90bd0f14dbfb8b7a7e671e0d501b793856c4ee0
Luca Annicchiarico <luca.annic@gmail.com>
2021-12-21 12:02:22 +0100

    working on contest php

--- php/contest.php
+++ php/contest.php
@@ -3,66 +3,98 @@
 session_start();
 
 include "login.php";
+
 use DB\DBAccess;
 
 function submitContest(&$htmlPage)
 {
     $messaggi = "";
-    $titolo = "";
-    $descrizione = "";
-    $linkyt = "";
-    $email = "";
 
     if (Login::is_logged()) {
 
-        $connection = new DBAccess();
-        $connectionOk = $connection->openDB();
+        if (isset($_POST["submit"])) {
+            $titolo = $_POST["titolo"];
+            $descrizione = $_POST["descrizione"];
+            $linkyt = $_POST["linkyt"];
+            $email = $_POST["email"];
+            $durata = $_POST["durata"];
+            $anno = $_POST["anno"];
+            $regista = $_POST["regista"];
+            $produttore = $_POST["produttore"];
+            $cast = $_POST["cast"];
 
-        if ($connectionOk) {
-            if (isset($_POST["submit"])) {
-                $titolo = $_POST["titolo"];
-                $descrizione = $_POST["descrizione"];
-                $linkyt = $_POST["linkyt"];
-                $email = $_POST["email"];
 
-                if (strlen($titolo) == 0) {
-                    $messaggi .= "<li>Titolo non presente</li>";
-                } elseif (preg_match('/\d/', $titolo)) {
-                    $messaggi .= "<li>Il nome non può contenere numeri</li>";
-                }
-
-                if (strlen($descrizione) == 0) {
-                    $messaggi .= "<li>Descrizione non presente</li>";
-                }
-
-                if (strlen($linkyt) == 0) {
-                    $messaggi .= "<li>Descrizione non presente</li>";
-                }
-
-                if (strlen($email) == 0) {
-                    $messaggi .= "<li>Descrizione non presente</li>";
-                }
-
-                if (strlen($messaggi) == "") {
-
-                    $connection = new DBAccess();
-                    $connectionOk = $connection->openDB();
-
-                    if($connectionOk){ // TODO
-                        $result = $connection->get("insert into Film(id, nome, descrizione, durata, anno, regista, produttore, cast, in_gara, approvato, candidatore) values (1, \"\");");
-                    }
-
-                    $messaggi .= "<p>Candidatura inviata con successo.</p>"
-    }
+            if (strlen($titolo) == 0) {
+                $messaggi = "<li>Titolo non present<li/>";
+            } elseif (preg_match('/\d/', $titolo)) {
+                $messaggi = "<li>Nome non può contenere numeri<li/>";
             }
-        } else {
-            $messaggi .= "<p>Errore connessione db</p>";
+
+            if (strlen($descrizione) == 0) {
+                $messaggi = "<li>Descrizione non presente<li/>";
+            }
+
+            if (strlen($linkyt) == 0) {
+                $messaggi = "<li>Link yt non presente<li/>";
+            }
+
+            if (strlen($email) == 0) {
+                $messaggi = "<li>Email non presente<li/>";
+            }
+
+            if (strlen($durata) == 0) {
+                $messaggi = "<li>Durata non presente<li/>";
+            }
+
+            if (strlen($anno) == 0) {
+                $messaggi = "<li>Anno non presente<li/>";
+            } elseif (preg_match('/^([SW])\w+([0-9]{4})$/', $titolo)) {
+                $messaggi = "<li>Anno può essere solo un numero di 4 cifre<li/>";
+            }
+
+            if (strlen($regista) == 0) {
+                $messaggi = "<li>Resgista non presente<li/>";
+            } elseif (preg_match('/\d/', $titolo)) {
+                $messaggi = "<li>Regista non può contenere numeri<li/>";
+            }
+
+            if (strlen($produttore) == 0) {
+                $messaggi = "<li>Produttore non presente<li/>";
+            } elseif (preg_match('/\d/', $titolo)) {
+                $messaggi = "<li>Produttore non può contenere numeri<li/>";
+            }
+
+            if (strlen($cast) == 0) {
+                $messaggi = "<li>Cast non presente<li/>";
+            } elseif (preg_match('/\d/', $titolo)) {
+                $messaggi = "<li>Cast non può contenere numeri<li/>";
+            }
+
+            if ($messaggi == "") {
+
+                $connection = new DBAccess();
+                $connectionOk = $connection->openDB();
+
+                if ($connectionOk) { // TODO
+                    if ($connection->get("insert into Film(id ,nome, descrizione, durata, anno, regista, produttore, cast, in_gara, approvato, candidatore) 
+                                          values (DEFAULT, '$titolo','$descrizione','$durata','$anno', '$regista', '$produttore', '$cast', 1, 0," . $_SESSION["login"] . ");")) {
+                        $messaggi = "<p>Film aggiunto con successo<p/>";
+                    } else {
+                        $messaggi = "<p>Errore nell aggiunta<p/>";
+                    }
+                    $connection->closeConnection();
+                } else {
+                    $messaggi = "<li>problemi db<li/>";
+                }
+            } else {
+                $messaggi = "<ul>" . $messaggi . "<ul/>";
+            }
         }
     } else { // not logged
-        $messaggi .= "<p>Utente non loggato</p>";
+        $messaggi .= "<li>Utente non loggato<li/>";
     }
 
-    $htmlPage =  str_replace("<listaBiglietti/>", $messaggi, $htmlPage);
+    $htmlPage = str_replace("<messaggi/>", $messaggi, $htmlPage);
 }
 
 if (isset($_POST["method"])) {
@@ -77,7 +109,7 @@ if (isset($_POST["method"])) {
 
     // show login/register/logout results
     Login::set_login_contents($htmlPage);
-    printBiglietti($htmlPage);
+    submitContest($htmlPage);
 
     $htmlPage = str_replace("placeholder", "style", $htmlPage);
 
