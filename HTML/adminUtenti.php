1f3ebe82b0aab21485d5d72a22dd6105a88f659c
Luca Annicchiarico <luca.annic@gmail.com>
2022-01-07 17:08:11 +0100

    init admin Utenti

---
+++ HTML/adminUtenti.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+
+include "../php/login.php";
+
+use DB\DBAccess;
+
+
+if (isset($_POST["method"])) {
+    // handle login/register/logout POST request
+    Login::handleLogin();
+
+    // redirect to same page (it will use GET request) https://en.wikipedia.org/wiki/Post/Redirect/Get
+    http_response_code(303);
+    header("Location: " . $_SERVER["REQUEST_URI"]);
+} else /* GET */ {
+    $htmlPage = file_get_contents("template/adminUtenti.html");
+
+    // show login/register/logout results
+    Login::printLogin($htmlPage);
+
+    echo $htmlPage;
+}
