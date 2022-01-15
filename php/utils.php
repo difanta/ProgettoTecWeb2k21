a0b6c72c5daa8810c90ef2e0dbe35af671e6cb0b
Luca Annicchiarico <luca.annic@gmail.com>
2022-01-15 16:12:39 +0100

    .

--- php/utils.php
+++ php/utils.php
@@ -6,6 +6,8 @@ class Utils
      * Prints POST function's user feedback
      * @used_in updateInfoUtente()
      * @used_in deleteCandidatura()
+     * @before_use check isset $_SESSION["method"] & $_SESSION["success"]
+     * @before_use check $_SESSION["method"] equals to the correct submit value
      * @param $htmlPage string html page
      * @param $placeholder string placeholder tag to be replaced with feedback
      */
