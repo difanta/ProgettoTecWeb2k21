b3020ed6a7a80f961093546826b917376c2a3443
Tommaso Di Fant <jackiedifant@gmail.com>
2022-01-26 11:07:06 +0100

    fix likes

--- php/db.php
+++ php/db.php
@@ -65,19 +65,19 @@ class DBAccess
         return $this->formatGetResult($stmt->get_result());
     }
 
-    public function insertLike($utente, $nomeFilm)
+    public function insertLike($utente, $idfilm)
     {
         $stmt = $this->connection->prepare("INSERT INTO _Like(utente, film) VALUES
-                                            (?, (SELECT id from Film where Film.nome = ?))");
-        $stmt->bind_param("ss", $utente, $nomeFilm);
+                                            (?, ?)");
+        $stmt->bind_param("si", $utente, $idfilm);
         return $stmt->execute();
     }
 
-    public function removeLike($utente, $nomeFilm)
+    public function removeLike($utente, $idfilm)
     {
         $stmt = $this->connection->prepare("DELETE from _Like
-                                            where utente = ? and film = (SELECT id from Film where Film.nome = ?)");
-        $stmt->bind_param("ss", $utente, $nomeFilm);
+                                            where utente = ? and film = ?");
+        $stmt->bind_param("si", $utente, $idfilm);
         return $stmt->execute();
     }
 
